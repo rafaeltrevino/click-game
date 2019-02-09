0d89179fb0261c1c1ee8422422087dbd0784f5d4
trevino <rafael.trevino@me.com>
2019-02-09 12:53:51 -0500

    Updates

---
+++ precache-manifest.942f4b97ad5acbae73cba6b51694a9e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1120cdbe1a51e380239",
+    "url": "/click-game/static/css/main.32bf61c4.chunk.css"
+  },
+  {
+    "revision": "b1120cdbe1a51e380239",
+    "url": "/click-game/static/js/main.b1120cdb.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/click-game/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "451cb8796073a9e8640b",
+    "url": "/click-game/static/js/runtime~main.451cb879.js"
+  },
+  {
+    "revision": "177072b836d68bb036d21acfeb80726b",
+    "url": "/click-game/index.html"
+  }
+];
